a74fe00c0b035c64037cf904cf90782ad56b8f95
Lakshamana <guitrompa1@gmail.com>
2019-08-12 17:31:04 -0300

    add validation messages for invalid pessoa

---
+++ src/main/java/com/arjuna/algamoney/algamoneyapi/service/exception/PessoaInvalidStateException.java
@@ -0,0 +1,6 @@
+package com.arjuna.algamoney.algamoneyapi.service.exception;
+
+public class PessoaInvalidStateException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+}
